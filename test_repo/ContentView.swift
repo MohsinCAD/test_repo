1aa1afe35087fcc7991349a3c9d46c12b4d49d64
Mohsin Ahmad <mohsin.ahmad@gmail.com>
2023-12-21 13:53:10 -0500

    Initial Commit

---
+++ test_repo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test_repo
+//
+//  Created by Mohsin on 2023-12-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ test_repo/test_repoApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_repoApp.swift
+//  test_repo
+//
+//  Created by Mohsin on 2023-12-21.
+//
+
+import SwiftUI
+
+@main
+struct test_repoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
